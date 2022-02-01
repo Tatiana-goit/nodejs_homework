453475ad3c5489f0bb96a647628a17a944c91316
Tatiana-goit <tanchou74@gmail.com>
2022-02-01 11:24:22 +0200

    add

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -22,6 +22,7 @@ const authenticate = async (req, res, next) => {
       message: 'Not authorized',
     })
   }
+  
   try {
     const {_id} = jwt.verify(token, SECRET_KEY)
     const user = await User.findById(_id)
